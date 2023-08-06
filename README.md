8a5b78940e2645ba35828ad9ad99d9c786ce29aa
aabchyein <abcqaz369@naver.com>
2023-08-06 15:31:30 +0900

    23-08-06  퀘스트 완

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 | -- | -- | -- | -- |
 | helloworld | print | [py](./helloworld.py) | -- |
 | syntaxs(문법) | 최소 문법 | [py](./syntaxs.py) | -- |
-| Data type(문자) | string | [py](./datatype_strings.py) | -- |
-| List | 리스트 | [py](./datatype_list.py) | -- |
+| Data type(Stiring) | 문자 | [py](./datatype_strings.py) | -- |
+| Data type(List) | 리스트 | [py](./datatype_list.py) | -- |
 | Loop | 반복문 | [py](./loops.py) | -- |
 | if문 | if/else if/else | [py](./ifelses.py) | -- |
 | input| 콘솔에서 값을 입력받을 때 | [py](./user_inputs.py) | -- |
 | Data type(Dictionary) | java의 hashmap과 같음 | [py](./datatype_dictionaris.py) | -- |
+| Data type(Datetime) | Datetime | [py](./datatype_datetimes.py) | -- |
+
 
--- loops_count.py
+++ loops_count.py
@@ -1,12 +1,24 @@
+# 퀘스트 
+# fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# user input을 받아 그 값만큼 fruits list의 값을 출력
+# Q 입력하면 while문 종료
+# <예시>
+# Loops count : 2
+# 1 : "apple"
+# 2 : "banana"
+# Loops count : Q  # 종료
+
 fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-
-count =int(input("Loops count : "))
-
-fruits_enumerate = enumerate(fruits)
+number = input("Loops count: ")
 fruit_print_format = "{0}: {1}"
 
-# while ()
-# for fruit in fruits
-#     if (count == 'Q') :
-#         break
-#     print(fruit_print_format.format(index, fruit))
+while number != "Q" :
+    count = int(number)
+    if (count == "Q") :
+        break
+    elif (0 < count <= len(fruits)) :
+        for (index, fruit) in enumerate(fruits[:count]) :
+            print(fruit_print_format.format(index+1, fruit))
+    else :
+        print("값을 다시 입력해주세요")
+    number = input("Loops count: ")
